c76e57691ff98f7b16134c9d59c066b90f9d1985
Minh Bui <tranduyminh.bui@gmail.com>
2019-09-16 23:07:58 +0930

    first commit

---
+++ backend/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is scream body",
+      createdAt: "2019-09-16T04:06:34.280Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
