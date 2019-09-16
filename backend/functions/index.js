f6534274ab603d91c5d2ba00a9001466540c4994
Minh Bui <tranduyminh.bui@gmail.com>
2019-09-16 23:17:25 +0930

    Update index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,15 +4,7 @@ const app = require("express")();
 
 admin.initializeApp();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB43zDkoj0YILiI65LglUKMWXgqCc_of0Q",
-  authDomain: "socialape-cdd8c.firebaseapp.com",
-  databaseURL: "https://socialape-cdd8c.firebaseio.com",
-  projectId: "socialape-cdd8c",
-  storageBucket: "socialape-cdd8c.appspot.com",
-  messagingSenderId: "1047075812915",
-  appId: "1:1047075812915:web:c8779bd3cd1c94d9c89546"
-};
+const firebaseConfig = {secrets};
 
 const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
